91abd2efb9bf3b2324a80994ab3af66ae09cd094
rizzok <38459379+rizzok@users.noreply.github.com>
2021-07-14 21:03:02 +0200

    Hello World!

---
+++ app.js
@@ -0,0 +1,12 @@
+class HelloMessage extends React.Component {
+
+  render () {
+    return <h1>Hello, world!</h1>
+  }
+  
+}
+
+ReactDOM.render(
+  <HelloMessage />,
+  document.getElementById('root')
+);
