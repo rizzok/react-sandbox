b7aefc5ded51a248fa5cb836d4d6c957070deba3
rizzok <38459379+rizzok@users.noreply.github.com>
2021-09-02 20:50:51 +0200

    creacte-react-app clean base

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import '../sass/App.scss';
+
+function Welcome() {
+  return (
+      <p>Bienvenue sur mon méga site !</p>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+}
+
+export default App;
