8eccaac33f6ca991ad1230f3ebeaf9f6f8444e71
rizzok <38459379+rizzok@users.noreply.github.com>
2021-09-11 18:19:34 +0200

    reinitialize project

--- src/components/TodoItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function TodoItem({ todo, onEditClick, onDeleteClick }) {
-  return (
-    <li key={todo.id}>
-      {todo.text}
-      <button onClick={() => onEditClick(todo)}>✏️</button>
-      <button onClick={() => onDeleteClick(todo.id)}>❌</button>
-    </li>
-  );
-}
