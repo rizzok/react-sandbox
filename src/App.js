735f2ac1dcbe42c9bf35ab1bac856ebc655f7133
rizzok <kevinrizzo13100@gmail.com>
2022-07-01 10:19:58 +0200

    Install Chakra UI and test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
+
 function App() {
-    return (
-        <div className="App">
-            <h1>React Sandbox</h1>
-        </div>
-    );
+  return (
+    <ChakraProvider>
+      <div className="App">
+        <Box mb={5}>
+          <h1>React Sandbox</h1>
+        </Box>
+        <Box m={5}>
+          <Button colorScheme="blue">Button</Button>
+        </Box>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
