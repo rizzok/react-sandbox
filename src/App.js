afa80047855e40ffb3b7b015abd38a6c4be9841b
rizzok <kevinrizzo13100@gmail.com>
2022-07-07 22:14:44 +0200

    OCR Redux P1C3 import and refactor code into React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import "./reducers/redux";
+import Score from "./components/Score";
+import { playPause } from "./reducers";
+
 function App() {
-    return (
-        <div className="App">
-            <h1>React Sandbox</h1>
+  const state = store.getState();
+
+  function handleClickPlayPause() {
+    store.dispatch(playPause());
+  }
+
+  return (
+    <div className="App">
+      <Score />
+      <div className="buttons">
+        <div className="buttons-row">
+          <button className="button" id="player-1">
+            Point Joueur 1
+          </button>
+          <button className="button" id="player-2">
+            Point Joueur 2
+          </button>
         </div>
-    );
+        <div className="buttons-row">
+          <button className="button" id="reset">
+            Remettre à zéro
+          </button>
+          <button className="button" id="pause" onClick={handleClickPlayPause}>
+            Pause / Reprendre
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { initialState, reducer } from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducer, initialState);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
