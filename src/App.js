b498df52c22a5c447c9279c2ce2ac467ba464bda
rizzok <kevinrizzo13100@gmail.com>
2022-07-08 15:51:11 +0200

    Remove Redux

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
-import "./reducers/redux";
-import Score from "./components/Score";
-import { playPause } from "./reducers";
-
 function App() {
-  const state = store.getState();
-
-  function handleClickPlayPause() {
-    store.dispatch(playPause());
-  }
 
   return (
     <div className="App">
-      <Score />
-      <div className="buttons">
-        <div className="buttons-row">
-          <button className="button" id="player-1">
-            Point Joueur 1
-          </button>
-          <button className="button" id="player-2">
-            Point Joueur 2
-          </button>
-        </div>
-        <div className="buttons-row">
-          <button className="button" id="reset">
-            Remettre à zéro
-          </button>
-          <button className="button" id="pause" onClick={handleClickPlayPause}>
-            Pause / Reprendre
-          </button>
-        </div>
-      </div>
+      <h1>React Sandbox</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { initialState, reducer } from "./reducers";
-
-const store = createStore(reducer, initialState);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
